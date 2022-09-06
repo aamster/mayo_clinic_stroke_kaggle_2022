863254adaf6b9ea4f467c13dd51f45b2de6cf69a
Adam Amster <adam.amster@alleninstitute.org>
2022-09-06 11:15:40 -0400

    reorg

---
+++ src/cli/modules/dataset_generator_runner.py
@@ -0,0 +1,24 @@
+import argschema
+
+from src.cli.schemas.dataset_generator import DatasetGeneratorSchema
+from src.tile_classifier.dataset_generator import DatasetGenerator
+
+
+class DatasetGeneratorRunner(argschema.ArgSchemaParser):
+    default_schema = DatasetGeneratorSchema
+
+    def run(self):
+        dataset_generator = DatasetGenerator(
+            tile_width=self.args['tile_width'],
+            tile_height=self.args['tile_height'],
+            fg_thresh=self.args['foreground_threshold'],
+            data_dir=self.args['data_dir']
+        )
+        dataset_generator.get_tiles(
+            meta_path=self.args['meta_path'],
+            out_path=self.args['out_path']
+        )
+
+
+if __name__ == '__main__':
+    DatasetGeneratorRunner().run()
