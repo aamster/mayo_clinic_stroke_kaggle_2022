eca6367b3eec8e0fbe131bc1f5cce907bbf284ce
Adam Amster <adam.amster@alleninstitute.org>
2022-09-11 16:05:16 -0400

    remove unused arg

--- src/stroke_classifier/cli/modules/dataset_generator_runner.py
+++ src/stroke_classifier/cli/modules/dataset_generator_runner.py
@@ -14,8 +14,7 @@ class DatasetGeneratorRunner(argschema.ArgSchemaParser):
             tile_width=self.args['tile_width'],
             tile_height=self.args['tile_height'],
             fg_thresh=self.args['foreground_threshold'],
-            data_dir=self.args['data_dir'],
-            downsample_factor=self.args['downsample_factor']
+            data_dir=self.args['data_dir']
         )
         dataset_generator.get_tiles(
             meta_path=self.args['meta_path'],
