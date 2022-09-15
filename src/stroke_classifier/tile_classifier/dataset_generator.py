99c9cc65c9ca4355e248e2d34d1aee2d1ec9fab5
Adam Amster <adam.amster@alleninstitute.org>
2022-09-15 15:22:08 -0400

    fix logic

--- src/stroke_classifier/tile_classifier/dataset_generator.py
+++ src/stroke_classifier/tile_classifier/dataset_generator.py
@@ -133,11 +133,9 @@ class DatasetGenerator:
             high = 0.8
             min, max = np.quantile(saturation, (low, high))
             if max == 0:
-                while max == 0 and high <= 1:
+                while max == 0 and high < 1:
                     high += .01
                     max = np.quantile(saturation, high)
-            if max == min:
-                return saturation
             saturation[saturation <= min] = min
             saturation[saturation >= max] = max
             saturation = \
