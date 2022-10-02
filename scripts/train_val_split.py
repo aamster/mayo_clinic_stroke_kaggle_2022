ae5ae60af5e882d7eca6cf8bdfebbde95f142522
Adam Amster <adam.amster@alleninstitute.org>
2022-10-02 12:36:51 -0400

    update split

--- scripts/train_val_split.py
+++ scripts/train_val_split.py
@@ -1,10 +1,12 @@
 import argparse
 import json
+from pathlib import Path
 
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--train_tiles_path', required=True)
+parser.add_argument('--train_slides_path', required=True)
+parser.add_argument('--train_slides_meta_path', required=True)
 parser.add_argument('--out_path', required=True)
 parser.add_argument('--train_frac', default=0.7, type=float)
 
@@ -12,26 +14,35 @@ args = parser.parse_args()
 
 
 def main():
-    with open(args.train_tiles_path) as f:
-        train_tiles = json.load(f)
+    with open(args.train_tiles_meta_path) as f:
+        train_slides = json.load(f)
 
-    train_tiles = [x for x in train_tiles if len(x['tile_coords']) > 0]
+    train_slides = [x for x in train_slides if len(x['tile_coords']) > 0]
+
+    for slide in train_slides:
+        slide_path = Path(slide['slide_path'])
+        # Update slide path to new path
+        slide['slide_path'] = \
+            str(Path(f'{args.train_slides_path}') / slide_path.stem)
 
     rng = np.random.default_rng(1234)
-    idxs = np.arange(len(train_tiles))
+    idxs = np.arange(len(train_slides))
     rng.shuffle(idxs)
     train_frac = args.train_frac
 
-    train = [train_tiles[idx] for idx in
-             idxs[:int(len(train_tiles) * train_frac)]]
-    val = [train_tiles[idx] for idx in
-           idxs[int(len(train_tiles) * train_frac):]]
+    train = [train_slides[idx] for idx in
+             idxs[:int(len(train_slides) * train_frac)]]
+    val = [train_slides[idx] for idx in
+           idxs[int(len(train_slides) * train_frac):]]
 
     with open(f'{args.out_path}/train_tiles.json', 'w') as f:
         f.write(json.dumps(train, indent=2))
 
     with open(f'{args.out_path}/val_tiles.json', 'w') as f:
         f.write(json.dumps(val, indent=2))
+
+    print(f'Train slides: {len(train)}')
+    print(f'Val slides: {len(val)}')
 
 
 if __name__ == '__main__':
